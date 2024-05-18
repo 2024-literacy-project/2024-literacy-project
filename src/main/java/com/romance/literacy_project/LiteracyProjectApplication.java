2480cd55a88e5a0a1320f95cef9de28be953b1fc
inredzero <caesete0524@naver.com>
2024-05-18 16:39:57 +0900

    first settings...

---
+++ src/main/java/com/romance/literacy_project/LiteracyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.romance.literacy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteracyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteracyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romance/literacy_project/LiteracyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romance.literacy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteracyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
